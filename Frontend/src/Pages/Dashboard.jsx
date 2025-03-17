3e8850d40faef03571b32e936f5f36dfc4426187
Parth arora <Partharora3068@gmail.com>
2025-03-17 22:58:08 +0530

    company name fix dashboard

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -18,8 +18,7 @@ const Dashboard = () => {
     const FetchCompany = async () => {
 
       const response = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/company/${uid}/${selectedCompany}`)
-      setOwnername(response.data.owner)
-
+    setOwnername(response.data.name);
     }
 
     FetchCompany()
